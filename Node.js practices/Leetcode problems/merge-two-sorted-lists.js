317982a1c97a198bd83479b899c50a6cf60dc542
Farrux Temurov <tfarrux70@gmail.com>
2025-08-26 21:41:28 +0500

    tested merging through helper functions for ListNode

--- Node.js practices/Leetcode problems/merge-two-sorted-lists.js
+++ Node.js practices/Leetcode problems/merge-two-sorted-lists.js
@@ -1,8 +1,32 @@
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+function buildList(arr) {
+    let dummy = new ListNode(-1);
+    let cur = dummy;
+    for (let val of arr) {
+        cur.next = new ListNode(val);
+        cur = cur.next;
+    }
+    return dummy.next;
+}
+
+function printList(list) {
+    let result = [];
+    while (list !== null) {
+        result.push(list.val);
+        list = list.next;
+    }
+    return result;
+}
+
 var mergeTwoLists = function (list1, list2) {
     let dummy = new ListNode(-1);
     let cur = dummy;
 
-    while (list1 != null && list2 != null) {
+    while (list1 !== null && list2 !== null) {
         if (list1.val > list2.val) {
             cur.next = list2;
             list2 = list2.next;
@@ -13,10 +37,12 @@ var mergeTwoLists = function (list1, list2) {
         cur = cur.next;
     }
 
-    cur.next = (list1 != null ? list1 : list2);
+    cur.next = list1 !== null ? list1 : list2;
     return dummy.next;
 };
 
-list1 = [1, 2, 4];
-list2 = [1, 3, 4];
-console.log(mergeTwoLists(list1, list2));
+let list1 = buildList([1, 2, 4]);
+let list2 = buildList([1, 3, 4]);
+
+let merged = mergeTwoLists(list1, list2);
+console.log(printList(merged)); // [1, 1, 2, 3, 4, 4]
