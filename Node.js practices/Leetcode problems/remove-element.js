b445dc09d32297b35e6b82d6bf141d1002b91dcb
Farrux Temurov <tfarrux70@gmail.com>
2025-08-28 16:30:34 +0500

    solution to remove element problem

---
+++ Node.js practices/Leetcode problems/remove-element.js
@@ -0,0 +1,18 @@
+var removeElement = function (nums, val) {
+    let left = 0;
+    let right = nums.length;
+
+    while (left < right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right - 1];
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return right;
+}
+
+nums = [0, 1, 2, 2, 3, 0, 4, 2];
+val = 2;
+console.log(removeElement(nums, val));
