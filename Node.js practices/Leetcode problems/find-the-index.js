65d55d9de286604e6ff6aa674492c1072d96e447
Farrux Temurov <tfarrux70@gmail.com>
2025-08-30 14:52:45 +0500

    Find the Index of the First Occurrence in a String solution with slicing approach

---
+++ Node.js practices/Leetcode problems/find-the-index.js
@@ -0,0 +1,10 @@
+// 28. Find the Index of the First Occurrence in a String
+
+var strStr = function (haystack, needle) {
+    for (let i = 0, j = needle.length; j <= needle.length; i++, j++) {
+        if (haystack.slice(i, j) === needle) {
+            return i;
+        }
+    }
+    return -1;
+}
