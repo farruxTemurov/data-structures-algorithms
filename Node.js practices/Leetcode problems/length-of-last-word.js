01976900bdb1100b1cb3bc7fda77c28708e4606f
Farrux Temurov <tfarrux70@gmail.com>
2025-09-01 15:08:59 +0500

    solution to length of last word problem with two-pointer approach

---
+++ Node.js practices/Leetcode problems/length-of-last-word.js
@@ -0,0 +1,17 @@
+// 58. Length of Last Word
+
+const lengthOfLastWord = function (s) {
+    let end = s.length - 1;
+
+    while (end >= 0 && s[end] === ' ') {
+        end--;
+    }
+
+    let start = end;
+
+    while (start >= 0 && s[start] !== ' ') {
+        start--;
+    }
+
+    return end - start;
+}
