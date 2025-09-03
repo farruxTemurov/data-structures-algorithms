e486851242470e2142f1ddffbf708aa3e258dd54
Farrux Temurov <tfarrux70@gmail.com>
2025-09-03 20:55:43 +0500

    testcases passed

--- Node.js practices/Leetcode problems/plus-one.js
+++ Node.js practices/Leetcode problems/plus-one.js
@@ -1,7 +1,7 @@
 //66. Plus One
 
 const plusOne = function (digits) {
-    for (let i = digits.lenght - 1; i >= 0; i++) {
+    for (let i = digits.length - 1; i >= 0; i++) {
         if (digits[i] + 1 !== 10) {
             digits[i] += 1;
             return digits;
@@ -16,3 +16,8 @@ const plusOne = function (digits) {
     }
 }
 
+digits = [1, 2, 3];
+console.log(plusOne(digits));
+
+digits = [4, 3, 2, 1];
+console.log(plusOne(digits));
