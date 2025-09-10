e8ceb32e547b05b34819a990c6f0e901b3bc9b21
Farrux Temurov <tfarrux70@gmail.com>
2025-09-10 21:59:15 +0500

    helper methods added to simulate linkedList

--- Node.js practices/Leetcode problems/remove-duplicates.js
+++ Node.js practices/Leetcode problems/remove-duplicates.js
@@ -1,5 +1,27 @@
 // 83. Remove Duplicates from Sorted List
 
+function arrayToList(arr) {
+    if (arr.length === 0) return null;
+    let head = { val: arr[0], next: null };
+    let current = head;
+    for (let i = 1; i < arr.length; i++) {
+        current.next = { val: arr[i], next: null };
+        current = current.next;
+    }
+    return head;
+}
+
+function listToArray(head) {
+    let arr = [];
+    let current = head;
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
+    }
+    return arr;
+}
+
+
 const deleteDuplicates = function (head) {
     let current = head;
 
@@ -13,4 +35,6 @@ const deleteDuplicates = function (head) {
     return head;
 };
 
-console.log(deleteDuplicates(head));
+let head = arrayToList([1, 1, 2]);
+let newHead = deleteDuplicates(head);
+console.log(listToArray(newHead)); 
