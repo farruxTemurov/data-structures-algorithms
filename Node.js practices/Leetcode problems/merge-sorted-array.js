bface05a58c1c4132435741ab54e46cc11038805
Farrux Temurov <tfarrux70@gmail.com>
2025-09-12 22:33:05 +0500

    testcase passed

--- Node.js practices/Leetcode problems/merge-sorted-array.js
+++ Node.js practices/Leetcode problems/merge-sorted-array.js
@@ -16,3 +16,10 @@ const merge = function (nums1, m, nums2, n) {
         right--;
     }
 }
+
+nums1 = [1, 2, 3, 0, 0, 0];
+m = 3;
+nums2 = [2, 5, 6];
+n = 3;
+merge(nums1, m, nums2, n);
+console.log(nums1);  // 👉 [1, 2, 2, 3, 5, 6]
