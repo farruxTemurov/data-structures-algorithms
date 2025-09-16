e44ec283a9b8e8b6eda463432d52d923a5cdd1e8
Farrux Temurov <tfarrux70@gmail.com>
2025-09-16 23:12:15 +0500

    algorithm for finding the largest of three numbers

--- Node.js practices/Practices by SariqDev/day-one.js
+++ Node.js practices/Practices by SariqDev/day-one.js
@@ -9,3 +9,20 @@ a = 2;
 b = 4;
 console.log(twoSum(a, b));
 
+// 2. Uch sondan eng kattasini topish
+
+const findTheLargest = function (a, b, c) {
+    if (a >= b && a >= c) {
+        console.log(a + " is largest");
+    } else if (b >= a && b >= c) {
+        console.log(b + " is largest");
+    } else {
+        console.log(c + " is largest");
+    }
+}
+a = 3;
+b = 1;
+c = 9;
+
+findTheLargest(a, b, c);
+
