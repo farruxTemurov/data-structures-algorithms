049867a2df811216e0ec0855f67300e8c8de9167
Farrux Temurov <tfarrux70@gmail.com>
2025-09-30 22:23:06 +0500

    function to find a connection

--- Node.js practices/Practices by SariqDev/day-ten.js
+++ Node.js practices/Practices by SariqDev/day-ten.js
@@ -1,7 +1,7 @@
 // Breadth-first search in grapghs
 // Maqsad: A va B orasida eng qisqa yo'lni topish
 
-const bfs = function (graph, start) {
+/*const bfs = function (graph, start) {
     let visited = new Set(); //keeps track of visited nodes
     let queue = [start]; // queue for bfs
     visited.add(start);
@@ -31,4 +31,43 @@ const graph = {
 };
 
 // Run BFS
-bfs(graph, "A");
+bfs(graph, "A");*/
+
+
+// function to find target person
+function search(graph, startNode, target = "elon musk") {
+    let queue = [...graph[startNode]]; // start with neighbors of startNode
+    let searched = new Set();          // track visited nodes
+
+    while (queue.length > 0) {
+        let person = queue.shift();    // dequeue
+
+        if (!searched.has(person)) {
+            if (person === target) {
+                console.log(`${target}ni topdik!`);
+                return true;
+            } else {
+                queue.push(...graph[person]); // add this person's neighbors
+                searched.add(person);
+            }
+        }
+    }
+
+    return false;
+}
+
+const graph = {
+    siz: ["ali", "vali", "tohir"],
+    ali: ["aziza", "olim"],
+    vali: ["botir", "ziyoda"],
+    tohir: ["elon musk", "mohir"],
+    olim: [],
+    aziza: [],
+    botir: [],
+    ziyoda: ["aziza"],
+    "elon musk": [],
+    mohir: []
+};
+
+console.log(search(graph, "siz"));
+console.log(search(graph, "ali", "vali"));   
