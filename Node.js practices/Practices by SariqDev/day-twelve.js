ba2d268b766e6b49e29cd79e07debce7e832dfad
Farrux Temurov <tfarrux70@gmail.com>
2025-10-02 22:05:30 +0500

    added comments for future reference

--- Node.js practices/Practices by SariqDev/day-twelve.js
+++ Node.js practices/Practices by SariqDev/day-twelve.js
@@ -2,6 +2,7 @@
 // Fractional Knapsack Problem
 
 function fractionalKnapsack(items, capacity) {
+    // sort the items by value/weight ratio (descending)
     items.sort((a, b) => (b.value / b.weight) - (a.value / a.weight));
 
     let totalValue = 0;
@@ -9,21 +10,23 @@ function fractionalKnapsack(items, capacity) {
 
     for (let item of items) {
         if (item.weight <= remainingCapacity) {
+            //take the whole item
             totalValue += item.value;
             remainingCapacity -= item.weight;
         } else {
+            //take the fraction that fits
             totalValue += item.value * (remainingCapacity / item.weight);
-            break;
+            break; //knapsack is full
         }
     }
     return totalValue;
 }
 
 const items = [
-    { value: 60, weight: 10 },
-    { value: 100, weight: 20 },
-    { value: 120, weight: 30 }
+    { value: 60, weight: 10 }, // gets the whole
+    { value: 100, weight: 20 }, // gets the whole
+    { value: 120, weight: 30 } // get a fraction -> 2/3
 ];
 
 const capacity = 50;
-console.log(fractionalKnapsack(items, capacity));
+console.log(fractionalKnapsack(items, capacity)); // output: 240
