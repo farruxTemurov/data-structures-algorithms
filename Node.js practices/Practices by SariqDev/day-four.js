90eca60ff2b0df09ed7044ef182d0df6f5e62633
Farrux Temurov <tfarrux70@gmail.com>
2025-10-04 23:01:47 +0500

    helper comment added

--- Node.js practices/Practices by SariqDev/day-four.js
+++ Node.js practices/Practices by SariqDev/day-four.js
@@ -3,10 +3,10 @@
 // Ma'lumotlar ro'yxatini saralash uchun eng kattasini/maydasini birinchiga qo'yadi, va shunday davom etib 
 // to'liq saralaydi. Hammasi bo'lib O(n^2) bo'ladi.
 
-
+/*
 const selectionSort = function (arr) {
     for (let i = 0; i < arr.length; i++) {
-        let minIndex = i; // you can change how it behaves here. if you want, you can select in descending order
+        let minIndex = i; // you can change how it behaves here. if you want, you can sort it in descending order
 
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[minIndex]) {
@@ -19,8 +19,31 @@ const selectionSort = function (arr) {
         }
     }
     return arr;
+};*/
+
+myList = [10, 2, 7, 3, 1];
+console.log(selectionSort(myList));
+
+// ascending order example
+
+const selectionSort = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let maxIndex = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        if (maxIndex !== j) {
+            [arr[i], arr[maxIndex]] = [arr[maxIndex], arr[i]];
+        }
+    }
+    return arr;
 };
 
 myList = [10, 2, 7, 3, 1];
 console.log(selectionSort(myList));
+
 
