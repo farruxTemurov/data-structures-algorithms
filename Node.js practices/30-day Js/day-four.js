5ae8b5d15fe25840ac32acdc7d01152635e647b8
Farrux Temurov <tfarrux70@gmail.com>
2025-10-06 21:36:25 +0500

    used for loop to transform each element in an array

---
+++ Node.js practices/30-day Js/day-four.js
@@ -0,0 +1,12 @@
+// Apply Transform Over Each Element in Array
+
+const map = function (arr, fn) {
+    const transformedArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        transformedArr[i] = fn(arr[i], i);
+    }
+
+    return transformedArrl;
+};
+
