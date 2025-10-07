ba7ec53da810f055393fbf90edbb3770d036ff8e
Farrux Temurov <tfarrux70@gmail.com>
2025-10-07 22:24:10 +0500

    filtered array of elements using truthy and falsy values

---
+++ Node.js practices/30-day Js/day-five.js
@@ -0,0 +1,12 @@
+// Filter Elements from Array
+
+const filter = function (arr, fn) {
+    const filteredArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
