481e74a5c71eed2123b3327fea1696eca2d60888
Farrux Temurov <tfarrux70@gmail.com>
2025-10-18 08:10:28 +0200

    added a function that limits after returning n times

--- Node.js practices/30-day Js/day-ten.js
+++ Node.js practices/30-day Js/day-ten.js
@@ -32,3 +32,26 @@ const mupltiply = (a, b, c) => (a * b * c);
 const mupltiplyOnce = once(mupltiply);
 const argsArray = [[5, 7, 4], [2, 3, 6], [4, 6, 8]];
 argsArray.forEach(args => console.log(mupltiplyOnce(...args)));
+
+// function that limits after returning n times
+const limitCalls = function (fn, n) {
+    let called = 0;
+    let result = [];
+
+    return function (...args) {
+        if (called < n) {
+            called += 1;
+            result = fn(...args);
+            return result;
+        } else {
+            return undefined;
+        }
+    }
+}
+
+const greet = name => `Hello, ${name}!`;
+const greetTwice = limitCalls(greet, 2);
+
+console.log(greetTwice("Alice")); // "Hello, Alice!"
+console.log(greetTwice("Bob"));   // "Hello, Bob!"
+console.log(greetTwice("Eve"));   // undefined
