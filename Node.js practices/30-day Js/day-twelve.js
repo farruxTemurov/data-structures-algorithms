f0a7dc063764c67ca8041adbfc8af4901135eca2
Farrux Temurov <tfarrux70@gmail.com>
2025-10-20 19:08:57 +0200

    solution for adding two promises

---
+++ Node.js practices/30-day Js/day-twelve.js
@@ -0,0 +1,7 @@
+// 2723. Add Two Promises
+
+const addTwoPromises = async function (promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+
+    return (value1 + value2);
+}
