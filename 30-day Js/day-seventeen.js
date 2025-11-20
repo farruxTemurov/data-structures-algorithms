cfd7070a572ca0d6df048a16708e08556410eae8
Farrux Temurov <tfarrux70@gmail.com>
2025-11-20 21:39:28 +0100

    add function test
    tested the main function with three cases

--- 30-day Js/day-seventeen.js
+++ 30-day Js/day-seventeen.js
@@ -5,7 +5,7 @@ var promiseAll = function (functions) {
         let results = new Array(functions.length);
         let count = 0;
 
-        functions.forEach(fn, i => {
+        functions.forEach((fn, i) => {
             fn()
                 .then(val => {
                     results[i] = val;
@@ -18,3 +18,45 @@ var promiseAll = function (functions) {
         });
     })
 }
+
+function wait(ms, value, shouldReject = false) {
+    return () =>
+        new Promise((res, rej) => {
+            setTimeout(() => {
+                if (shouldReject) rej(value);
+                else res(value);
+            }, ms)
+        })
+}
+
+async function test() {
+    const funcs1 = [
+        wait(300, "A"),
+        wait(100, "B"),
+        wait(200, "C")
+    ];
+
+    console.log("Test 1 (should resolve['A','B','C']):");
+    promiseAll(funcs1)
+        .then(res => console.log("Resolved:", res))
+        .catch(err => console.log("Rejected:", err));
+
+    const funcs2 = [
+        wait(100, "X"),
+        wait(200, "Y", true),   // reject
+        wait(50, "Z")
+    ];
+
+    console.log("\nTest 2 (should reject with 'Y'):");
+    promiseAll(funcs2)
+        .then(res => console.log("Resolved:", res))
+        .catch(err => console.log("Rejected:", err));
+
+    // --- Test 3: empty array ---
+    console.log("\nTest 3 (should resolve []):");
+    promiseAll([])
+        .then(res => console.log("Resolved:", res))
+        .catch(err => console.log("Rejected:", err));
+}
+
+test();
