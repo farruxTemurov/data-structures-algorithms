5d6b18de86ecc09518347216838b078d35335a4f
Farrux Temurov <tfarrux70@gmail.com>
2025-11-27 21:40:24 +0100

    add a function to return chunked array

---
+++ 30-day Js/day-nineteen.js
@@ -0,0 +1,13 @@
+// 2677. Chunk Array
+// js slice() method takes in 2 args: start(inclusive) and end(exclusive)
+
+const chunk = function (arr, size) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+
+    return result;
+}
+
