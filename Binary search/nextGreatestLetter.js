f184293489cefc689ede06fc2a8a12c56e925f22
Farrux Temurov <tfarrux70@gmail.com>
2025-12-01 11:35:14 +0100

    add nextGreatestLetter function to find the smallest letter greater than target

---
+++ Binary search/nextGreatestLetter.js
@@ -0,0 +1,18 @@
+// 744. Find Smallest Letter Greater Than Target
+
+const nextGreatestLetter = function (letters, target) {
+    let left = 0, right = letters.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (letters[mid] <= target) { // we want to skip all letters that are NOT strictly greater than the target
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return letters[left % letters.length]; // If a valid element is found:
+    // left is already inside the array, modulo does nothing.
+    // else: modulo pushes it back to 0 so that we return the first element of the arr(as stated in the constraint)
+}
