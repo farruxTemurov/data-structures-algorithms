6854a5181d724b78dd9b902fd9dfa2152a5b1fec
Farrux Temurov <tfarrux70@gmail.com>
2025-12-03 19:34:27 +0100

    add countNegatives function to count negative numbers in a sorted matrix

---
+++ Leetcode problems/countNegatives.js
@@ -0,0 +1,14 @@
+// 1351. Count Negative Numbers in a Sorted Matrix
+
+const countNegatives = function (grid) {
+    let negativeNums = [];
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) { // if a number in row's column is negative
+                negativeNums.push(grid[i][j]);
+            }
+        }
+    }
+    return negativeNums.length;
+}
