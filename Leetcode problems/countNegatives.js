5435501401cdac7c3d372dfd83e14c979896f43c
Farrux Temurov <tfarrux70@gmail.com>
2025-12-03 19:36:17 +0100

    add example usage for countNegatives function

--- Leetcode problems/countNegatives.js
+++ Leetcode problems/countNegatives.js
@@ -12,3 +12,7 @@ const countNegatives = function (grid) {
     }
     return negativeNums.length;
 }
+
+let grid =
+    [[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]];
+console.log(countNegatives(grid));
