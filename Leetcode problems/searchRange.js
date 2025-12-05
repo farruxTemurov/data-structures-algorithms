9c1563e1862bb2d7e0527da9722e6c03e693ffaa
Farrux Temurov <tfarrux70@gmail.com>
2025-12-05 20:14:21 +0100

    add searchRange function to find first and last position of element in sorted array

---
+++ Leetcode problems/searchRange.js
@@ -0,0 +1,17 @@
+// 34. Find First and Last Position of Element in Sorted Array
+
+var searchRange = function (nums, target) {
+    let positionOfTarget = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            positionOfTarget.push(i);
+        }
+    }
+    if (positionOfTarget.length === 0) { // if target isn't found
+        return [-1, -1];
+    }
+
+    return [positionOfTarget[0], positionOfTarget[positionOfTarget.length - 1]]; // returns the start and the end index
+    // if there are more than two indexes, it gets the first and last.
+}
