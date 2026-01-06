a1ce58a632636e65ad719d010a2c4d412eb74fd2
Farrux Temurov <tfarrux70@gmail.com>
2026-01-06 20:44:59 +0100

    add another approach of the code

--- 30-day Js/day-twenty.js
+++ 30-day Js/day-twenty.js
@@ -2,9 +2,19 @@
 /*Every array in JavaScript inherits methods from Array.prototype.
 This is part of JavaScript’s prototype-based inheritance.*/
 
-Array.prototype.last = function () {
+/*Array.prototype.last = function () {
     return this.length ? this[this.length - 1] : -1;
+}*/
+
+// using if condition
+Array.prototype.last = function () {
+    if (this.length === 0) return -1;
+    return this[this.length - 1];
 }
 
+
 const arr = [null, {}, 3];
-console.log(arr.last());
+console.log(arr.last()); // output: 3
+
+const nums = [];
+console.log(nums.last()); // output: -1
