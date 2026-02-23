88b8add9904205e9c150f4c524a1c755800f7f09
Farrux Temurov <tfarrux70@gmail.com>
2026-02-23 21:06:42 +0100

    add countSubarraySum function to count subarrays that sum to a given value

--- practicing concepts/prefixSum.js
+++ practicing concepts/prefixSum.js
@@ -10,3 +10,25 @@ const prefixSum = function (nums) {
 
 const nums = [2, -2, 3, 0];
 console.log(prefixSum(nums));
+
+const countSubarraySum = function (nums, k) {
+    const n = nums.length;
+    const prefix = new Array(n + 1).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
+    }
+
+    let count = 0;
+    for (let i = 0; i < prefix.length; i++) {
+        for (let j = i + 1; j < prefix.length; j++) {
+            // If the difference between two prefix sums equals K, 
+            // the subarray between them sums to K!
+            if (prefix[j] - prefix[i] === k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
